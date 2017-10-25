b2769c49e7c1bbdd0f95b7e9a4044ee576333591
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-10-25 10:47:34 -0400

    Add support for automatic registering of RequestLocalizationOptions #6

--- src/ForEvolve.AspNetCore.Localization/ForEvolveLocalizationOptions.cs
+++ src/ForEvolve.AspNetCore.Localization/ForEvolveLocalizationOptions.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Localization;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Localization;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,10 @@ namespace ForEvolve.AspNetCore.Localization
 {
     public class ForEvolveLocalizationOptions : LocalizationOptions
     {
+        public ForEvolveMvcLocalizationOptions MvcOptions { get; set; }
+
+        //public IList<CultureInfo> SupportedCultures { get; set; }
+        //public CultureInfo DefaultCulture { get; set; }
+        public RequestLocalizationOptions RequestLocalizationOptions { get; set; }
     }
 }
--- src/ForEvolve.AspNetCore.Localization/StartupExtensions.cs
+++ src/ForEvolve.AspNetCore.Localization/StartupExtensions.cs
@@ -1,9 +1,14 @@
 ﻿using ForEvolve.AspNetCore.Localization;
 using ForEvolve.AspNetCore.Localization.Adapters;
 using ForEvolve.AspNetCore.Localization.Resources;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -11,6 +16,9 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class ForEvolveLocalizationStartupExtensions
     {
         public const string DefaultResourcesPath = "Resources";
+
+        private static ForEvolveLocalizationOptions Options { get; set; }
+        private static ILocalizationValidationMetadataProvider ValidationMetadataProvider { get; set; }
 
         public static IServiceCollection AddForEvolveLocalization(this IServiceCollection services)
         {
@@ -20,33 +28,24 @@ namespace Microsoft.Extensions.DependencyInjection
 
         public static IServiceCollection AddForEvolveLocalization(this IServiceCollection services, Action<ForEvolveLocalizationOptions> setupAction)
         {
+            // Localization Options
+            var supportedCultures = new List<CultureInfo>(new[]
+            {
+                new CultureInfo("en"),
+                new CultureInfo("fr"),
+            });
+            var defaultCulture = supportedCultures.First();
             var localizationOptions = new ForEvolveLocalizationOptions
             {
-                ResourcesPath = DefaultResourcesPath
-            };
-            setupAction(localizationOptions);
-            return services
-                .AddLocalization(options =>
+                ResourcesPath = DefaultResourcesPath,
+                MvcOptions = new ForEvolveMvcLocalizationOptions
                 {
-                    options.ResourcesPath = localizationOptions.ResourcesPath;
-                });
-        }
-
-        public static IMvcBuilder AddForEvolveMvcLocalization(this IMvcBuilder mvcBuilder)
-        {
-            return mvcBuilder
-                .AddForEvolveMvcLocalization(options => { });
-        }
-
-        public static IMvcBuilder AddForEvolveMvcLocalization(this IMvcBuilder mvcBuilder, Action<ForEvolveMvcLocalizationOptions> setupAction)
-        {
-            // Setup the options
-            var localizationOptions = new ForEvolveMvcLocalizationOptions
-            {
-                EnableDataAnnotationsLocalization = true,
-                EnableViewLocalization = true,
-                ConfigureValidationMetadataProvider = provider => { },
-                DefaultAdapterOptions = new ForEvolveMvcDefaultLocalizationAdapterOptions()
+                    EnableDataAnnotationsLocalization = true,
+                    EnableViewLocalization = true,
+                    ConfigureValidationMetadataProvider = provider => { },
+                    DefaultAdapterOptions = new ForEvolveMvcDefaultLocalizationAdapterOptions()
+                },
+                RequestLocalizationOptions = CreateDefaultRequestLocalizationOptions(defaultCulture, supportedCultures)
             };
             setupAction(localizationOptions);
 
@@ -55,32 +54,83 @@ namespace Microsoft.Extensions.DependencyInjection
                 // Custom multi-messages adapter that duplicate the attribute logic
                 // A better solution is welcome :)
                 new StringLengthLocalizationValidationAttributeAdapter(),
-                
-                // Keep this one last
-                new DefaultLocalizationValidationAttributeAdapter(localizationOptions.DefaultAdapterOptions)
-            );
-            localizationOptions.ConfigureValidationMetadataProvider(defaultValidationMetadataProvider);
 
-            // Add the new ValidationMetadataProvider to MVC
+                // Keep this one last
+                new DefaultLocalizationValidationAttributeAdapter(localizationOptions.MvcOptions.DefaultAdapterOptions)
+            );
+            localizationOptions.MvcOptions
+                .ConfigureValidationMetadataProvider(defaultValidationMetadataProvider);
+
+            // Regiter services
+            services
+                .AddSingleton(localizationOptions)
+                //.AddSingleton<ILocalizationValidationMetadataProvider>(defaultValidationMetadataProvider)
+                .AddLocalization(options =>
+                {
+                    options.ResourcesPath = localizationOptions.ResourcesPath;
+                });
+
+            // Keep global references
+            Options = localizationOptions;
+            ValidationMetadataProvider = defaultValidationMetadataProvider;
+
+            return services;
+        }
+
+        public static IMvcBuilder AddForEvolveMvcLocalization(this IMvcBuilder mvcBuilder)
+        {
+            // Add the ValidationMetadataProvider to MVC
             mvcBuilder
                  .AddMvcOptions(options =>
                 {
-                    options.ModelMetadataDetailsProviders.Add(defaultValidationMetadataProvider);
+                    options.ModelMetadataDetailsProviders.Add(ValidationMetadataProvider);
                 });
 
             // Add the default ViewLocalization (opt-out basis)
-            if (localizationOptions.EnableViewLocalization)
+            if (Options.MvcOptions.EnableViewLocalization)
             {
                 mvcBuilder.AddViewLocalization();
             }
 
             // Add the default DataAnnotationsLocalization (opt-out basis)
-            if (localizationOptions.EnableDataAnnotationsLocalization)
+            if (Options.MvcOptions.EnableDataAnnotationsLocalization)
             {
                 mvcBuilder.AddDataAnnotationsLocalization();
             }
 
             return mvcBuilder;
+        }
+
+
+        private static RequestLocalizationOptions CreateDefaultRequestLocalizationOptions(CultureInfo defaultCulture, IList<CultureInfo> supportedCultures)
+        {
+            var requestLocalizationOptions = new RequestLocalizationOptions
+            {
+                // State what the default culture for your application is. This will be used if no specific culture
+                // can be determined for a given request.
+                DefaultRequestCulture = new RequestCulture(defaultCulture, defaultCulture),
+
+                // You must explicitly state which cultures your application supports.
+                // These are the cultures the app supports for formatting numbers, dates, etc.
+                SupportedCultures = supportedCultures,
+
+                // These are the cultures the app supports for UI strings, i.e. we have localized resources for.
+                SupportedUICultures = supportedCultures
+            };
+
+            // You can change which providers are configured to determine the culture for requests, or even add a custom
+            // provider with your own logic. The providers will be asked in order to provide a culture for each request,
+            // and the first to provide a non-null result that is in the configured supported cultures list will be used.
+            // By default, the following built-in providers are configured:
+            // - QueryStringRequestCultureProvider, sets culture via "culture" and "ui-culture" query string values, useful for testing
+            // - CookieRequestCultureProvider, sets culture via "ASPNET_CULTURE" cookie
+            // - AcceptLanguageHeaderRequestCultureProvider, sets culture via the "Accept-Language" request header
+            //requestLocalizationOptions.RequestCultureProviders.Insert(0, new CustomRequestCultureProvider(async context =>
+            //{
+            //  // My custom request culture logic
+            //  return new ProviderCultureResult("en");
+            //}));
+            return requestLocalizationOptions;
         }
     }
 }
