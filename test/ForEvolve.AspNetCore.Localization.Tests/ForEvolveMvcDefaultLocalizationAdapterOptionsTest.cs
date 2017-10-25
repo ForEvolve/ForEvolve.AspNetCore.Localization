fcf2f05d48166c7bdc6f88380e7f4ae553ff84e8
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-10-24 21:19:55 -0400

    Added ForEvolveMvcDefaultLocalizationAdapterOptions tests

--- test/ForEvolve.AspNetCore.Localization.Tests/ForEvolveMvcDefaultLocalizationAdapterOptionsTest.cs
+++ test/ForEvolve.AspNetCore.Localization.Tests/ForEvolveMvcDefaultLocalizationAdapterOptionsTest.cs
@@ -7,17 +7,41 @@ namespace ForEvolve.AspNetCore.Localization
 {
     public class ForEvolveMvcDefaultLocalizationAdapterOptionsTest
     {
-        [Fact]
-        public void TODO()
+        public class Ctor
         {
-            // Arrange
+            [Fact]
+            public void Should_assign_DefaultSupportedAttributes_to_SupportedAttributes()
+            {
+                // Act
+                var sut = new ForEvolveMvcDefaultLocalizationAdapterOptions();
 
+                // Assert
+                Assert.Equal(sut.DefaultSupportedAttributes, sut.SupportedAttributes);
+            }
 
-            // Act
+            [Fact]
+            public void Should_assign_specified_supportedAttributes_to_SupportedAttributes()
+            {
+                // Arrange
+                var supportedAttributes = new string[] {
+                    "SomeAttribute",
+                    "SomeOtherAttribute"
+                };
 
+                // Act
+                var sut = new ForEvolveMvcDefaultLocalizationAdapterOptions(supportedAttributes);
 
-            // Assert
-            throw new NotImplementedException();
+                // Assert
+                Assert.Equal(supportedAttributes, sut.SupportedAttributes);
+            }
+
+            [Fact]
+            public void Should_throw_ArgumentNullException_when_specified_supportedAttributes_are_null()
+            {
+                // Act & Assert
+                var ex = Assert.Throws<ArgumentNullException>(() => new ForEvolveMvcDefaultLocalizationAdapterOptions(null));
+                Assert.Equal("supportedAttributes", ex.ParamName);
+            }
         }
     }
 }
