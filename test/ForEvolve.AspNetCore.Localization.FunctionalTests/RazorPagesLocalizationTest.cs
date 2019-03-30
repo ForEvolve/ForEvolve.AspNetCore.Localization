8e3c95ee017b3fdee66d0eb6cf0aa1d09d2f4ea1
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-03-30 14:03:09 -0400

    Fix typo and update test

--- test/ForEvolve.AspNetCore.Localization.FunctionalTests/RazorPagesLocalizationTest.cs
+++ test/ForEvolve.AspNetCore.Localization.FunctionalTests/RazorPagesLocalizationTest.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -33,51 +34,55 @@ namespace ForEvolve.AspNetCore.Localization
             ));
         }
 
-        [Theory]
-        [InlineData("fr")]
-        public async Task When_the_form_is_submitted_messages_should_be_translated(string culture)
+        public class French : RazorPagesLocalizationTest
         {
-            // Arrange
-            var uri = $"/?culture={culture}";
-            var parser = new HtmlParser();
-            var client = _factory.CreateClient();
-            var config = Configuration.Default.WithDefaultLoader();
+            public French(WebApplicationFactory<Startup> factory) : base(factory) { }
 
-            var model = new IndexModel.ValidationModel();
-            var jsonModel = JsonConvert.SerializeObject(model);
-            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonModel);
+            [Fact]
+            public async Task When_the_form_is_submitted_messages_should_be_translated()
+            {
+                // Arrange
+                var uri = "/?culture=fr";
+                var parser = new HtmlParser();
+                var client = _factory.CreateClient();
+                var config = Configuration.Default.WithDefaultLoader();
 
-            var formContent = new FormUrlEncodedContent(dictionary.Select(kv 
-                => new KeyValuePair<string, string>($"Model.{kv.Key}", kv.Value)));
+                var model = new IndexModel.ValidationModel();
+                var jsonModel = JsonConvert.SerializeObject(model);
+                var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonModel);
 
-            // Post the "form"
-            var response = await client.PostAsync(uri, formContent);
+                var formContent = new FormUrlEncodedContent(dictionary.Select(kv 
+                    => new KeyValuePair<string, string>($"Model.{kv.Key}", kv.Value)));
 
-            // Parse the response
-            var pageContent = await response.Content.ReadAsStringAsync();
-            var document = await parser.ParseDocumentAsync(pageContent);
-            var summary = document.GetElementById("validationSummary");
+                // Post the "form"
+                var response = await client.PostAsync(uri, formContent);
 
-            // Find the summary messages
-            var listItems = summary.GetElementsByTagName("li");
-            var errorMessages = listItems.Select(x => x.TextContent);
+                // Parse the response
+                var pageContent = await response.Content.ReadAsStringAsync();
+                var document = await parser.ParseDocumentAsync(pageContent);
+                var summary = document.GetElementById("validationSummary");
 
-            // Assert
-            Assert.Collection(errorMessages,
-                message => Assert.Equal("Les champs 'Compare1' et 'Compare2' ne sont pas identiques.", message),
-                message => Assert.Equal("Le champ CreditCard est une numéro de carte de crédit invalide.", message),
-                message => Assert.Equal("Le champ EmailAddress est une adresse courriel invalide.", message),
-                message => Assert.Equal("Le champ FileExtensions accepte uniquement les fichiers aux extensions suivantes: .png, .jpg, .jpeg, .gif", message),
-                message => Assert.Equal("Le champ MaxLength doit avoir une longueur maximum de '5'.", message),
-                message => Assert.Equal("Le champ MinLength doit avoir une longueur minimum de '5'.", message),
-                message => Assert.Equal("Le champ Phone est un numéro de téléphone invalide.", message),
-                message => Assert.Equal("Le champ Range doit être entre 10 et 20.", message),
-                message => Assert.Equal("Le champ RegularExpression doit correspondre à l'expression régulière '[a-z]'.", message),
-                message => Assert.Equal("Le champ Required est requis.", message),
-                message => Assert.Equal("Le champ StringLength doit être composé d'un maximum de 5 caractères.", message),
-                message => Assert.Equal("Le champ StringLengthIncludingMinimum doit être entre 3 et 5 caractères.", message),
-                message => Assert.Equal("Le champ Website n'est pas une URL pleinement qualifiée valide, commençant par HTTP, HTTP ou FTP.", message)
-            );
+                // Find the summary messages
+                var listItems = summary.GetElementsByTagName("li");
+                var errorMessages = listItems.Select(x => x.TextContent);
+
+                // Assert
+                Assert.Collection(errorMessages,
+                    message => Assert.Equal("Les champs 'Compare1' et 'Compare2' ne sont pas identiques.", message),
+                    message => Assert.Equal("Le champ CreditCard est un numéro de carte de crédit invalide.", message),
+                    message => Assert.Equal("Le champ EmailAddress est une adresse courriel invalide.", message),
+                    message => Assert.Equal("Le champ FileExtensions accepte uniquement les fichiers aux extensions suivantes: .png, .jpg, .jpeg, .gif", message),
+                    message => Assert.Equal("Le champ MaxLength doit avoir une longueur maximum de '5'.", message),
+                    message => Assert.Equal("Le champ MinLength doit avoir une longueur minimum de '5'.", message),
+                    message => Assert.Equal("Le champ Phone est un numéro de téléphone invalide.", message),
+                    message => Assert.Equal("Le champ Range doit être entre 10 et 20.", message),
+                    message => Assert.Equal("Le champ RegularExpression doit correspondre à l'expression régulière '[a-z]'.", message),
+                    message => Assert.Equal("Le champ Required est requis.", message),
+                    message => Assert.Equal("Le champ StringLength doit être composé d'un maximum de 5 caractères.", message),
+                    message => Assert.Equal("Le champ StringLengthIncludingMinimum doit être entre 3 et 5 caractères.", message),
+                    message => Assert.Equal("Le champ Website n'est pas une URL pleinement qualifiée valide, commençant par HTTP, HTTP ou FTP.", message)
+                );
+            }
         }
     }
 }
