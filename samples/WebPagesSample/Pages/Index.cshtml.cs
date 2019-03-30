c8cb4c6b13eb81429b431eea61250372c710197c
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-03-30 01:43:11 -0400

    Setup failing tests related to #21

--- samples/WebPagesSample/Pages/Index.cshtml.cs
+++ samples/WebPagesSample/Pages/Index.cshtml.cs
@@ -8,39 +8,57 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebPagesSample.Pages
 {
+    // IgnoreAntiforgeryToken allows tests to post here without worrying about anti-forgery
+    [IgnoreAntiforgeryToken(Order = 1001)]
     public class IndexModel : PageModel
     {
-        [Compare(nameof(Compare2))]
-        public string Compare1 { get; set; } = "Some value";
-        public string Compare2 { get; set; } = "Some non-matching value";
-        [CreditCard]
-        public string CreditCard { get; set; } = "Not a credit card";
-        [EmailAddress]
-        public string EmailAddress { get; set; } = "not an email address";
-        [FileExtensions]
-        public string FileExtensions { get; set; } = "toto.unknown";
-        [MaxLength(5)]
-        public string MaxLength { get; set; } = "ABCDEF";
-        [MinLength(5)]
-        public string MinLength { get; set; } = "ABC";
-        [Phone]
-        public string Phone { get; set; }
-        [Range(10, 20)]
-        public int Range { get; set; } = 30;
-        [RegularExpression("[a-z]")]
-        public string RegularExpression { get; set; } = "1";
-        [Required]
-        public string Required { get; set; }
-        [StringLength(5)]
-        public string StringLength { get; set; } = "ABCDEF";
-        [StringLength(5, MinimumLength = 3)]
-        public string StringLengthIncludingMinimum { get; set; } = "AB";
-        [Url]
-        public string Url { get; set; } = "Not an URL";
+        [BindProperty]
+        public ValidationModel Model { get; set; } = new ValidationModel();
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            return Page();
+        }
 
+        public IActionResult OnPost()
+        {
+            var form = Request.Form;
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return Redirect("/?success=true");
+        }
+
+        public class ValidationModel
+        {
+            [Compare(nameof(Compare2))]
+            public string Compare1 { get; set; } = "Some value";
+            public string Compare2 { get; set; } = "Some non-matching value";
+            [CreditCard]
+            public string CreditCard { get; set; } = "Not a credit card";
+            [EmailAddress]
+            public string EmailAddress { get; set; } = "not an email address";
+            [FileExtensions]
+            public string FileExtensions { get; set; } = "toto.unknown";
+            [MaxLength(5)]
+            public string MaxLength { get; set; } = "ABCDEF";
+            [MinLength(5)]
+            public string MinLength { get; set; } = "ABC";
+            [Phone]
+            public string Phone { get; set; } = "not a phone";
+            [Range(10, 20)]
+            public int Range { get; set; } = 30;
+            [RegularExpression("[a-z]")]
+            public string RegularExpression { get; set; } = "1";
+            [Required]
+            public string Required { get; set; }
+            [StringLength(5)]
+            public string StringLength { get; set; } = "ABCDEF";
+            [StringLength(5, MinimumLength = 3)]
+            public string StringLengthIncludingMinimum { get; set; } = "AB";
+            [Url]
+            public string Website { get; set; } = "Not an URL";
         }
     }
 }
